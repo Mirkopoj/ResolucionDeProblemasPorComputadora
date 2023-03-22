e494c4c93233ffee99737cd6d8685c64390e5f44
Mirkopoj <mirkopoj@hotmail.com>
2023-03-22 12:20:24 -0300

    Operaciones robustas

---
+++ operaciones_de_matrices_dinamicas/Include/operaciones.h
@@ -0,0 +1,21 @@
+#ifndef OPS_MATRIX_H
+#define OPS_MATRIX_H
+#include <stdio.h>
+
+typedef struct matrix {
+	int rows;
+	int cols;
+	int **data;
+} matrix ;
+
+void matrix_save(FILE *stream, matrix mat);
+int matrix_get(FILE *stream, matrix *mat);
+int add(matrix mat1, matrix mat2, matrix *res);
+int transpose(matrix mat, matrix *res);
+int scalar_mult(matrix mat, int scalar, matrix *res);
+int matrix_mult(matrix mat1, matrix mat2, matrix *res);
+void matrix_save_setpretty(void);
+int matrix_alloc(matrix *mat);
+void matrix_free(matrix *mat);
+
+#endif // !DEBUG
---
+++ operaciones_de_matrices_dinamicas/src/mains/main.c
@@ -0,0 +1,78 @@
+#include "../../Include/operaciones.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+	FILE *fi = stdin;
+	FILE *fo = stdout;
+	for (int i=1; i<argc; i++) {
+		if(!strcmp(argv[i],"-i")){
+			i++;
+			if(i==argc){
+				fprintf(stderr, "-i miss used, should be \"-i <PATH-TO-INPUT-FILE>\"\n");
+				exit(-1);
+			}
+			fi = fopen(argv[i], "r");
+			if (!fi) {
+				fprintf(stderr, "fi: file failed to open");
+			}
+		}
+		if(!strcmp(argv[i],"-o")){
+			i++;
+			if(i==argc){
+				fprintf(stderr, "-o miss used, should be \"-o <PATH-TO-OUTPUT-FILE>\"\n");
+				exit(-1);
+			}
+			fo = fopen(argv[i], "w+");
+			if (!fo) {
+				fprintf(stderr, "fo: file failed to open");
+			}
+		}
+		if(!strcmp(argv[i],"-p")){
+			matrix_save_setpretty();
+		}
+	}
+
+	matrix mat1;
+	matrix mat2;
+
+	if (fi==stdin) printf("Ingrese valores de la matriz 1\n"); 
+	matrix_get(fi, &mat1);
+	if (fi==stdin) printf("Ingrese valores de la matriz 2\n"); 
+	matrix_get(fi, &mat2);
+	if (fi!=stdin) fclose(fi);
+
+	if (fo==stdout) printf("Mat1:\n");
+	matrix_save(fo, mat1);
+	if (fo==stdout) printf("Mat2:\n");
+	matrix_save(fo, mat2);
+
+	matrix ret;
+	ret.rows = mat1.rows;
+	ret.cols = mat1.cols;
+	matrix_alloc(&ret);
+
+	if (fo==stdout) printf("Mat1 + Mat2:\n");
+	add(mat1, mat2, &ret);
+	matrix_save(fo, ret);
+
+	if (fo==stdout) printf("[Mat1]^T:\n");
+	transpose(mat1, &ret);
+	matrix_save(fo, ret);
+
+	if (fo==stdout) printf("Mat1 * 5:\n");
+	scalar_mult(mat1, 5, &ret);
+	matrix_save(fo, ret);
+
+	if (fo==stdout) printf("Mat1 * Mat2:\n");
+	matrix_mult(mat1, mat2, &ret);
+	matrix_save(fo, ret);
+
+	matrix_free(&mat1);
+	matrix_free(&mat2);
+	matrix_free(&ret);
+	if (fo!=stdout) fclose(fo);
+	return 0;
+}
