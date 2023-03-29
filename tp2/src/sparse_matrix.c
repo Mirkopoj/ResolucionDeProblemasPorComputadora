30d4159fe53cfc4e440b90940e9a11705b66643a
Mirko <mirkopoj@hotmail.com>
2023-03-29 14:35:05 -0300

    funciones sparse

--- tp2/src/sparse_matrix.c
+++ tp2/src/sparse_matrix.c
@@ -42,11 +42,23 @@ int enlarge(matrix mat){
 	struct element *new_data = realloc(mat.data, element_capacity(mat)*2);
 	if (!new_data) { return -1; }
 	mat.data = new_data;
+	mat.num_elements *= 2;
 	return 0;
+}
+
+void shrunk(matrix mat){
+	struct element *new_data = realloc(mat.data, element_capacity(mat)/2);
+	if (!new_data) { return; }
+	mat.data = new_data;
+	mat.num_elements /= 2;
 }
 
 void increase_count(matrix mat){
 	mat.assigned_elements++;
+}
+
+void decrement_count(matrix mat){
+	mat.assigned_elements--;
 }
 
 void element_swap(struct element a, struct element b){
@@ -90,4 +102,39 @@ int (*element_setter)(matrix, coordinate, int) = set_element_bin;
 
 int set_element(matrix mat, coordinate cord, int elem){
 	return element_setter(mat, cord, elem);
+}
+
+// Element deleters
+void delete_element_bin(matrix mat, coordinate cord){
+	int sptr;
+	if (bin_search(mat, cord, &sptr) < 0) { return; }
+	for (int i=sptr; i<element_count(mat)-1; i++) {
+		element_swap(mat.data[i], mat.data[i+1]);
+	}
+	decrement_count(mat);
+	if (element_capacity(mat) > 2*element_count(mat)) {
+		shrunk(mat);	
+	}
+}
+
+void (*element_deleter)(matrix, coordinate) = delete_element_bin;
+
+void errase_element(matrix mat, coordinate cord){
+	element_deleter(mat, cord);
+}
+
+int rows(matrix mat){
+	return mat.rows;
+}
+
+int cols(matrix mat){
+	return mat.cols;
+}
+
+int element_count(matrix mat){
+	return mat.assigned_elements;
+}
+
+int element_capacity(matrix mat){
+	return mat.num_elements;
 }
